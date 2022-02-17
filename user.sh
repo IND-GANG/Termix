6b18d5f44e3117ac264bf2c4a93cf415b751369d
ADIL <90197148+IND-GANG@users.noreply.github.com>
2022-02-17 22:30:46 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,24 +2,25 @@
 clear
 echo " "
 echo " "
-echo -e "\e[33mEnter Your Banner Name(e.g.  ADIL) \e[32m \n\n"
+echo -e "\e[33mEnter Your Banner Name \e[32m \n\n"
 read varbanner
 
 echo " " 
 
-echo "toilet -f ivrit '           $varbanner'  -F gay | lolcat" > t-ban.txt
-
-echo -e "\e[33mEnter Your Name \e[32m  \n\n"
-read varname
+echo "toilet -f ivrit '               $varbanner'  -F gay | lolcat" > t-ban.txt
 
 echo " "
 echo " "
-echo -e "\e[33m Enter Your terminal name(e.g. ROOT,LINUX or KALI etc....) \e[32m \n\n"
+echo -e "\e[33m Enter Your terminal name  ( e.g. Adil@ROOT ) \e[32m \n\n"
 echo " "
 read varterm
 echo " " 
 
-echo "PS1='\033[01;34m\]└─\[\033[01;32m\]$varname\[\033[01;34m\]@\[\033[01;31m\]$varterm\[\033[00;34m\]\[\033[01;34m\]==>\[\033[01;32m\]' " > ash.txt
+echo "PS1='\[\e[1;34m
+\a┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
+\a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
+\a├─[\033[1;32m\w\033[1;34m]\033[1;34m
+\[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\][~]:#\[\e[1;32m\] ' " > ash.txt
 
 cat "t-ban.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
